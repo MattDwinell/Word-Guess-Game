65f533dcc7b24d6bc598b04d9ce5ae434c814d62
Matt D <dwinell.matt@gmail.com>
2019-03-27 01:17:29 -0400

    fifth commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-var wordBank = ["ossified","etiolated","exudations","shambolic","psoriatic","syncresis","syzygy","ineluctable","eructations","sciolist","imbroglio","agglutination","bathetic","pusillanimous","nacreous","morbific"] ;
+var wordBank = ["ossified","etiolated","exudations","shambolic","psoriatic","syncretic","syzygy","ineluctable","eructations","sciolist","imbroglio","agglutination","bathetic","pusillanimous","nacreous","morbific","tumuli","sephardic","recrudescence","amanuensis","onanistic","wen","lethean"] ;
+var wordDefinitions = ["to have been turned into bone","to lose substance, to become feeble","fluids emitted by an organism through a pore or a wound","chaotic or disorganized","of psoriasis, characterized by itchiness and flaking skin","a union of two different religions","a pair of connected or corresponding things. Also, a connjuction or opposition, especially of the sun","inescapable, unable to be avoided","burps","someone who pretends to be knowledgeable or well-informed","an extremely confused, complicated, or embrassing situation","the clumping of particles","anticlimactic, dissapointing","cowardly, showing a lack of courage","pearlescent, having a color similiar to that of a pearl","disease causing","ancient burial mounds or barrows. the singular form is tumulus","Pertaining to a subset of Jews whose ancestors lived in Spain","the recurrence of an undesirable condition","a literary or artistic assitant, often one who copies manuscripts","pertaining to masturbation. pointless, self-absorbed, self-congratulatory","Nowadays known as a boil or swelling on the skin, it used to mean an extremely large or crowded city","Causing oblivion or forgetfulness of the past"]
 var letterPlaceHolder =[document.getElementById("letter0"), document.getElementById("letter1"),document.getElementById("letter2"),document.getElementById("letter3"),document.getElementById("letter4"),document.getElementById("letter5"),document.getElementById("letter6"),document.getElementById("letter7"),document.getElementById("letter8"),document.getElementById("letter9"),document.getElementById("letter10"),document.getElementById("letter11"),document.getElementById("letter12"),document.getElementById("letter13"),document.getElementById("letter14")];
 var userGuess = "";
 var winCount = 0;
@@ -9,6 +10,8 @@ var lettersToGuess="";
 var lettersLeftToGuess = "";
 var userGuesses = [];
 var correctGuesses = 0;
+var repeat=false;
+var definition =""
 var winCountPlaceholder =document.getElementById("wincount");
 var lossCountPlaceholder = document.getElementById("losscount");
 var lettersGuessedPlaceholder = document.getElementById("lettersguessed");
@@ -16,16 +19,28 @@ var guessesPlaceholder = document.getElementById("guessesleft");
 var wordPlaceHolder = document.getElementById("wordtoguess");
 document.getElementById("startbutton").addEventListener("click", gameStart);
 function gameStart(){
+     repeat = false;
+    userGuess="";
+    correctGuesses =0;
+    userGuesses =[];
+    numGuesses=0;
+    for (i =0 ; i<letterPlaceHolder.length; i++){
+        letterPlaceHolder[i].textContent ="";
+        letterPlaceHolder[i].style.color="red";
+    }
+    document.getElementById("winlossscreen").style.display="none";
     document.getElementById("startdisplay").style.display= "none";
     document.getElementById("gamescaffold").style.display= "block";
-         wordToGuess = wordBank[Math.floor(Math.random()*wordBank.length)];
+    var wordChooser = Math.floor(Math.random()*wordBank.length);
+    definition = wordDefinitions[wordChooser];
+         wordToGuess = wordBank[wordChooser];
         numGuesses = (wordToGuess.length + 5);
         wordPlaceHolder.textContent =(wordToGuess);
         lettersToGuess = wordToGuess;
        lettersToGuess.split(" ");
        lettersLeftToGuess = lettersToGuess;
         guessesPlaceholder.textContent = ( numGuesses );
-        lettersGuessedPlaceholder.textContent = ("Letters Used: none");
+        lettersGuessedPlaceholder.textContent = "none";
         winCountPlaceholder.textContent = (winCount);
         lossCountPlaceholder.textContent=(lossCount);
         for (i=0 ; i<wordToGuess.length; i++){
@@ -34,7 +49,9 @@ letterPlaceHolder[i].textContent = lettersToGuess[i];
     }
 function game(){
     userGuess = event.key;
+    userGuess=userGuess.toLowerCase();
     userGuesses.push (userGuess);
+    
     lettersGuessedPlaceholder.textContent = userGuesses.join(" ");
     for (i=0; i<wordToGuess.length; i++){
         if (userGuess == lettersToGuess[i]){
@@ -45,17 +62,37 @@ correctGuesses ++
         numGuesses -=1;
         guessesPlaceholder.textContent = (numGuesses);
     
-
-    console.log(userGuesses);
-    console.log(lettersLeftToGuess);
-    if ((correctGuesses >=wordToGuess.length) || (numGuesses <= 0)){
-        if (correctGuesses >=wordToGuess.length){
+    if ((correctGuesses >= wordToGuess.length) || (numGuesses == 0)){
+        if (correctGuesses == wordToGuess.length){
+        
+        document.getElementById("winorlose").textContent = "You win!"
+        winCount ++;
+        } else {
+            
+            document.getElementById("winorlose").textContent = "You lose!";
+            lossCount++;
+        }
         document.getElementById("gamescaffold").style.display ="none";
         document.getElementById("winlossscreen").style.display="block";
-        }
+        document.getElementById("endword").textContent = wordToGuess;
+        document.getElementById("definition").textContent = definition;
     }
     
 }
 document.onkeyup = function(event){
+    userGuess = event.key;
+    userGuess = userGuess.toLowerCase();
+    repeat =false;
+    if ((event.keyCode >= 65 && event.keyCode <= 90) && (wordToGuess.length > 0)){
+        for (i=0; i<userGuesses.length;i++){
+            if (userGuess==userGuesses[i]){
+                repeat=true;
+            }
+        }
+        if (repeat == false){
     game();
+        }
+    }
 }
+document.getElementById("restartbutton").addEventListener("click", gameStart);
+
